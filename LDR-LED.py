ee96294a081f54cd7e428db71b0b37297407de55
chatchaiokub <chatty30433@windowslive.com>
2016-09-22 00:25:35 +0000

    fix

--- LDR-LED.py
+++ LDR-LED.py
@@ -1,38 +1,57 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
-import os
 
 GPIO.setmode(GPIO.BCM)
 
-#My addition for LEDs
+#########################
+
 BLUE = 17
 RED = 27
-LDR = 4
 GPIO.setup(BLUE, GPIO.OUT)
 GPIO.setup(RED, GPIO.OUT)
-#End my code
+
+LDR = 4
+
+#########################
 
 def RCtime (LDR):
+
   reading = 0
+
   GPIO.setup(LDR, GPIO.OUT)
   GPIO.output(LDR, GPIO.LOW)
   time.sleep(0.1)
   
   GPIO.setup(LDR, GPIO.IN)
-  # This takes about 1 millisecond per loop cycle
   while (GPIO.input(LDR) == GPIO.LOW):
     reading += 1
   return reading
   
 while True:
-  print RCtime(LDR) # Read RC timing using pin #18
-  
-  #Begin my code for the LEDs
-  if RCtime(LDR) <2100:
-    GPIO.output(RED, False)
-    GPIO.output(BLUE, False)
-  else:
-    GPIO.output(RED, True)
-    GPIO.output(BLUE, True)
-  
+  print RCtime(LDR)
+
+#########################
+
+z = 0
+
+if z==0:
+	if RCtime(LDR) > 10000:
+		GPIO.output(RED, True)
+    		GPIO.output(BLUE, True)
+		time.sleep(5)
+		GPIO.output(RED, False)
+		GPIO.output(BLUE, False)
+		z=1
+	else:
+		GPIO.output(RED, False)
+		GPIO.output(BLUE, False)
+elif z==1:
+	if RCtime(LDR) < 10000:
+		z = 0
+	else:
+		GPIO.output(RED, False)
+		GPIO.output(BLUE, False)
+
+	
+  	  
