95bc0d74fe2ce3cbe2c0adc3e8e2375f69981060
chatchaiokub <chatty30433@windowslive.com>
2016-09-25 15:37:07 +0000

    POST API from Sheetsu to Google Sheet

--- Blink.py
+++ Blink.py
@@ -5,14 +5,14 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 
-GPIO.setup(4,GPIO.OUT)
+GPIO.setup(17,GPIO.OUT)
 GPIO.setup(27,GPIO.OUT)
 
 while 1:
 
-	GPIO.output(4,GPIO.HIGH)
+	GPIO.output(17,GPIO.HIGH)
 	GPIO.output(27,GPIO.HIGH)
 	time.sleep(1)
-	GPIO.output(4,GPIO.LOW)
+	GPIO.output(17,GPIO.LOW)
 	GPIO.output(27,GPIO.LOW)
 	time.sleep(1)
